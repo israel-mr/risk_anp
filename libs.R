378beb90b0bccb62c7231c585f3cbc890b886b5c
israel-mr <0israelmr@gmail.com>
2024-10-22 10:11:50 -0600

    first upload: 22Oct2024

---
+++ libs.R
@@ -0,0 +1,32 @@
+
+#update.packages(ask = FALSE)
+#rm(list = ls())
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(cluster)  # clustering
+library(factoextra)  # Vis-clustering
+library(caret)  # ml
+library(randomForest)
+
+library(highcharter)
+
+#tables+
+library(kableExtra)
+library(DT) #interactive table
+library(scales)
+library(readxl)
+
+ 
+library(readxl)
+library(tidyverse)
+library(corrplot)
+library(FactoMineR)
+library(factoextra)
+ 
+
+library(RMySQL)  # O RMariaDB
+
+library(DT)
+library(dplyr)
+library(readxl)
